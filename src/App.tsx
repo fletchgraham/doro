9343baf98641aaa36f2194ce83cdf6ad9d3275b8
fletchgraham <sfletchgraham@gmail.com>
2026-01-17 09:52:32 -0700

    show list of tasks

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,19 @@ import "./App.css";
 import Countdown from "react-countdown";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [date, setDate] = useState(Date.now() + 20 * 60000);
-
   const handleReset = () => setDate(Date.now() + 20 * 60000);
+  const [tasks, setTasks] = useState(["an important task", "another task"]);
 
   return (
     <>
       <h1>
         <Countdown date={date} />
       </h1>
-      <p>Some very important task.</p>
-      <div className="card">
-        <button onClick={handleReset}>Reset</button>
-      </div>
+      <button onClick={handleReset}>Reset</button>
+      {tasks.map((task) => {
+        return <p key={task}>{task}</p>;
+      })}
     </>
   );
 }
