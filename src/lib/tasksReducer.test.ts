57572403835217d845eb3a901e45ff38f9b8309b
fletchgraham <sfletchgraham@gmail.com>
2026-01-20 15:45:41 -0700

    switch to discriminated union

--- src/lib/tasksReducer.test.ts
+++ src/lib/tasksReducer.test.ts
@@ -1,4 +1,5 @@
 import tasksReducer from "./tasksReducer";
+import { createTask } from "./tasksReducer";
 import type Task from "../types/Task";
 import { expect, test } from "vitest";
 
@@ -6,8 +7,25 @@ test("adds a new task to the list of tasks", () => {
   const tasks: Task[] = [];
   const newTasks = tasksReducer(tasks, {
     type: "ADD_TASK",
-    args: { text: "hey" },
+    text: "hey",
   });
   expect(newTasks.length).toBe(1);
   expect(newTasks[0].text).toBe("hey");
+});
+
+test("remove a task", () => {
+  const tasks: Task[] = [
+    createTask("foo"),
+    createTask("bar"),
+    createTask("baz"),
+  ];
+
+  const newTasks = tasksReducer(tasks, {
+    type: "REMOVE_TASK",
+    taskId: tasks[1].id,
+  });
+
+  expect(newTasks[0].text).toBe("foo");
+  expect(newTasks[1].text).toBe("baz");
+  expect(newTasks.length).toBe(2);
 });
--- src/lib/tasksReducer.ts
+++ src/lib/tasksReducer.ts
@@ -1,13 +1,12 @@
 import type Task from "../types/Task";
 
-interface TasksAction {
-  type: string; // I think this should be union of all of the actions I wrote out
-  args: any; // not sure how to handle this because each action needs different info
-}
+type TasksAction =
+  | { type: "ADD_TASK"; text: string }
+  | { type: "REMOVE_TASK"; taskId: string };
 
 // for now this is duplicated from the hooks file, will delete the other when
 // refactor is complete.
-const createTask = (text: string): Task => {
+export const createTask = (text: string): Task => {
   return {
     text: text,
     notes: "",
@@ -22,9 +21,10 @@ const createTask = (text: string): Task => {
 const tasksReducer = (state: Task[], action: TasksAction) => {
   switch (action.type) {
     case "ADD_TASK":
-      return [...state, createTask(action.args.text)];
+      return [...state, createTask(action.text)];
+    case "REMOVE_TASK":
+      return state.filter((t) => t.id !== action.taskId);
   }
-  return state;
 };
 
 export default tasksReducer;
